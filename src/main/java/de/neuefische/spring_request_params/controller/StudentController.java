8dcd1132a68b1d84f727ef77b45e2d59f6008da5
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-09-21 12:48:40 +0200

    Add StatusException to StudentController.deleteStudent

--- src/main/java/de/neuefische/spring_request_params/controller/StudentController.java
+++ src/main/java/de/neuefische/spring_request_params/controller/StudentController.java
@@ -4,7 +4,9 @@ package de.neuefische.spring_request_params.controller;
 import de.neuefische.spring_request_params.model.Student;
 import de.neuefische.spring_request_params.repo.StudentRepo;
 import de.neuefische.spring_request_params.service.StudentService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +33,11 @@ public class StudentController {
 
     @DeleteMapping("{id}")
     public void deleteStudent(@PathVariable String id) {
-        studentService.delete(id);
+        try {
+            studentService.delete(id);
+        }
+        catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
     }
 }
